57362ae0f498cdee186c61c68ec684beeb67f2ba
Gabriel Bull <gavroche.bull@gmail.com>
2013-12-30 15:25:07 -0500

    Changed method to get classname and namespace from worker files

--- src/GearmanHandler/Daemon.php
+++ src/GearmanHandler/Daemon.php
@@ -186,33 +186,25 @@ class Daemon
      */
     private function getClassNameFromFile($file)
     {
-        $fp = fopen($file, 'r');
-        $class = $namespace = $buffer = '';
+        $fileContent = file_get_contents($file);
+        $class = $namespace = '';
         $i = 0;
-        while (!$class) {
-            if (feof($fp)) break;
-
-            $buffer .= fread($fp, 512);
-            $tokens = token_get_all($buffer);
-
-            if (strpos($buffer, '{') === false) continue;
-
-            for (; $i < count($tokens); $i++) {
-                if ($tokens[$i][0] === T_NAMESPACE) {
-                    for ($j = $i + 1; $j < count($tokens); $j++) {
-                        if ($tokens[$j][0] === T_STRING) {
-                            $namespace .= '\\' . $tokens[$j][1];
-                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
-                            break;
-                        }
+        $tokens = token_get_all($fileContent);
+        for (; $i < count($tokens); $i++) {
+            if ($tokens[$i][0] === T_NAMESPACE) {
+                for ($j = $i + 1; $j < count($tokens); $j++) {
+                    if ($tokens[$j][0] === T_STRING) {
+                        $namespace .= '\\' . $tokens[$j][1];
+                    } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                        break;
                     }
                 }
+            }
 
-                if ($tokens[$i][0] === T_CLASS) {
-                    for ($j = $i + 1; $j < count($tokens); $j++) {
-                        if ($tokens[$j] === '{') {
-                            $class = $tokens[$i + 2][1];
-                        }
+            if ($tokens[$i][0] === T_CLASS) {
+                for ($j = $i + 1; $j < count($tokens); $j++) {
+                    if ($tokens[$j] === '{') {
+                        $class = $tokens[$i + 2][1];
                     }
                 }
             }
