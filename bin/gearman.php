c036b2f8a973536f3c7ed3a30a126b9174ee9692
Gabriel Bull <gavroche.bull@gmail.com>
2014-01-13 21:18:06 -0500

    Added verification to see if file exists before loading them

--- bin/gearman.php
+++ bin/gearman.php
@@ -8,7 +8,11 @@ if (!function_exists("pcntl_fork")) {
     trigger_error("The function pcntl_fork was not found. Please ensure Process Control functions are installed", E_USER_ERROR);
 }
 
-(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+}
 
 use Symfony\Component\Console\Application;
 use GearmanHandler\Command\Start as StartCommand;
